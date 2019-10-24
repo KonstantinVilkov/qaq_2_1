6d044590e779fb538c4a3a13803eeb49d55100ad
KonstantinVilkov <hothak@yandex.ru>
2019-10-24 20:15:37 +0300

    Init commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,45 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    void shouldRerurn100IfAmountIs900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+    @Test
+
+    void shouldRerurn0IfAmountIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRerurn1000IfAmountIs0() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRerurn500IfAmountIs1500() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1500;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+
+}
